a18ba2fa9ccbcf8eec8dcfef83899f418fd5ac96
David Ferreira <ferreiradavid361@gmail.com>
2024-01-04 15:54:04 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,49 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
+  },
+  redirects() {
+    return [
+      {
+        source: "/components",
+        destination: "/docs/components/accordion",
+        permanent: true,
+      },
+      {
+        source: "/docs/components",
+        destination: "/docs/components/accordion",
+        permanent: true,
+      },
+      {
+        source: "/examples",
+        destination: "/examples/mail",
+        permanent: false,
+      },
+      {
+        source: "/docs/primitives/:path*",
+        destination: "/docs/components/:path*",
+        permanent: true,
+      },
+      {
+        source: "/figma",
+        destination: "/docs/figma",
+        permanent: true,
+      },
+      {
+        source: "/docs/forms",
+        destination: "/docs/components/form",
+        permanent: false,
+      },
+      {
+        source: "/docs/forms/react-hook-form",
+        destination: "/docs/components/form",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,52 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+import { Playlist } from '../data/playlists';
+
+interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
+  playlists: Playlist[];
+}
+
+export function Sidebar({ className, playlists }: SidebarProps) {
+  return (
+    <div className={cn(className, 'h-full')}>
+      <div className="space-y-4 py-4">
+        <div className="py-2">
+          <h2 className="relative px-7 text-lg font-semibold tracking-tight">
+            Playlists
+          </h2>
+          <ScrollArea className="px-1 h-[87vh]">
+            <div className="space-y-1 p-2">
+              {playlists?.map((playlist, i) => (
+                <Button
+                  key={`${playlist}-${i}`}
+                  variant="ghost"
+                  className="w-full justify-start font-normal"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2 h-4 w-4"
+                  >
+                    <path d="M21 15V6" />
+                    <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                    <path d="M12 12H3" />
+                    <path d="M16 6H3" />
+                    <path d="M12 18H3" />
+                  </svg>
+                  {playlist}
+                </Button>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+      </div>
+    </div>
+  );
+}
